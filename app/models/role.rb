31c5cf4c89ed3a673f7b9579058007dce4e6116b
enzo-gos <enzo.nguyen.gos@gmail.com>
2024-04-12 13:33:58 +0700

    fix: validate roles

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,14 +1,19 @@
 class Role < ApplicationRecord
-  has_and_belongs_to_many :users, :join_table => :users_roles
-  
+  NAMES = %w[admin user].freeze
+
+  has_and_belongs_to_many :users, join_table: :users_roles
+
   belongs_to :resource,
-             :polymorphic => true,
-             :optional => true
-  
+             polymorphic: true,
+             optional: true
 
   validates :resource_type,
-            :inclusion => { :in => Rolify.resource_types },
-            :allow_nil => true
+            inclusion: { in: Rolify.resource_types },
+            allow_nil: true
+
+  validates :name,
+            presence: true,
+            inclusion: { in: NAMES }
 
   scopify
 end
