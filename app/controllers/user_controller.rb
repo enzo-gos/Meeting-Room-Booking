5080a5884de5dc0af69c321ad774819210164901
enzo-gos <enzo.nguyen.gos@gmail.com>
2024-04-16 09:15:04 +0700

    feat: user profile change password

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,24 @@
 class UserController < ApplicationController
-  def profile
+  before_action :authenticate_user!
+
+  def index
     add_breadcrumb 'User Profile', user_profile_path
     @user_profile = current_user
+  end
+
+  def update
+    @user_profile = current_user
+    if @user_profile.update(user_profile_params)
+      render :index, notice: 'Profile updated successfully.', status: :created
+    else
+      @user_profile.errors.add(:current_password, 'is incorrect')
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_profile_params
+    params.require(:user).permit(:current_password, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   rolify
+  has_one_attached :avatar
   after_create :assign_default_role
 
   # Include default devise modules. Others available are:
@@ -8,13 +9,28 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable,
          omniauth_providers: [:google_oauth2]
 
+  validate :current_password_is_correct,
+  if: :validate_password?, on: :update
+
   has_many :teams, through: :user_teams
 
   def self.from_google(user_params)
     create_with(uid: user_params[:uid], password: Devise.friendly_token[0, 20], provider: 'google').find_or_create_by!(email: user_params[:email])
   end
 
+  def current_password_is_correct
+    if User.find(id).valid_password?(current_password) == false
+      errors.add("Current password is incorrect.")
+    end
+  end
+
+  def validate_password?
+    !password.blank?
+  end
+
   def assign_default_role
     add_role(:user) if roles.blank? || !has_role?(:user)
   end
+
+  attr_accessor :current_password
 end
