4e8dc343ca5e6de6493266886716ccbe76230e36
enzo-gos <enzo.nguyen.gos@gmail.com>
2024-04-25 11:58:30 +0700

    feat: list, filter room

---
+++ app/controllers/concerns/filterable.rb
@@ -0,0 +1,23 @@
+module Filterable
+  def filter!(resource)
+    store_filters(resource)
+    [session["#{resource.to_s.underscore}_filters"], apply_filters(resource)]
+  end
+
+  private
+
+  def store_filters(resource)
+    session["#{resource.to_s.underscore}_filters"] = {} unless session.key?("#{resource.to_s.underscore}_filters")
+
+    session["#{resource.to_s.underscore}_filters"].merge!(filter_params_for(resource))
+  end
+
+  def filter_params_for(resource)
+    filter_params = params.transform_values { |value| value.is_a?(Array) ? value.reject(&:empty?).join(',') : value }
+    filter_params.permit(resource::FILTER_PARAMS)
+  end
+
+  def apply_filters(resource)
+    resource.filter(session["#{resource.to_s.underscore}_filters"])
+  end
+end
---
+++ app/javascript/controllers/room_filter_form_controller.js
@@ -0,0 +1,59 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="room-filter-form"
+export default class extends Controller {
+  static targets = ["form", "filterBtn", "filterOption"];
+
+  connect() {
+    this.activeFilter();
+    $("body").on("click", (event) => {
+      this.closeDropdown();
+    });
+  }
+
+  hasFilter() {
+    return this.filterOptionTargets.some((el) => el.checked === true);
+  }
+
+  activeFilter() {
+    if (this.hasFilter()) {
+      $(this.filterBtnTarget).removeClass("text-secondary-500");
+      $(this.filterBtnTarget).addClass(
+        "text-primary-800 border border-primary-600"
+      );
+
+      $(this.filterBtnTarget).children().removeClass("text-black");
+      $(this.filterBtnTarget).children().addClass("text-primary-800");
+    } else {
+      $(this.filterBtnTarget).removeClass(
+        "text-primary-800 border-primary-600"
+      );
+      $(this.filterBtnTarget).addClass("text-secondary-500");
+
+      $(this.filterBtnTarget).children().removeClass("text-primary-800");
+      $(this.filterBtnTarget).children().addClass("text-black");
+    }
+  }
+
+  filter() {
+    clearTimeout(this.timeout);
+    this.activeFilter();
+
+    this.timeout = setTimeout(() => {
+      this.formTarget.requestSubmit();
+    }, 300);
+  }
+
+  keepDropdownOpen(event) {
+    event.stopPropagation();
+    $(".facility_dropdown").addClass("open");
+  }
+
+  stopPropagation(event) {
+    event.stopPropagation();
+  }
+
+  closeDropdown(event) {
+    $(".facility_dropdown").removeClass("open");
+  }
+}
