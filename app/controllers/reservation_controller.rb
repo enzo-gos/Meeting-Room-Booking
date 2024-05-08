cdcc570ba71ddd49ba0b864c4787036df569e2ed
enzo-gos <enzo.nguyen.gos@gmail.com>
2024-05-08 14:59:32 +0700

    refactor: init_reservation before action, rubocop

--- app/controllers/reservation_controller.rb
+++ app/controllers/reservation_controller.rb
@@ -1,11 +1,10 @@
 class ReservationController < ApplicationController
   before_action :init_breadcrumbs
+  before_action :init_reservation
 
   def edit
-    add_breadcrumb params[:id]
     add_breadcrumb 'Edit'
 
-    @meeting_reservation = MeetingReservation.find(params[:id])
     @user_list = User.where.not(id: current_user.id)
 
     @selected_rule = @meeting_reservation.rule_to_option
@@ -14,10 +13,8 @@ class ReservationController < ApplicationController
   def show
     add_breadcrumb params[:id]
 
-    @meeting_reservation = MeetingReservation.find(params[:id])
     @meeting_room = @meeting_reservation.room
     @members = @meeting_reservation.members
-    @user_list = User.where.not(id: current_user.id)
 
     @selected_rule = @meeting_reservation.rule_to_option
   end
@@ -26,5 +23,10 @@ class ReservationController < ApplicationController
 
   def init_breadcrumbs
     add_breadcrumb 'Reservation'
+    add_breadcrumb params[:id]
+  end
+
+  def init_reservation
+    @meeting_reservation = MeetingReservation.find(params[:id])
   end
 end
