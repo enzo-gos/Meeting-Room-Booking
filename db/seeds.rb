4de10d2f269a3ce16cab09692d97d60a1689e342
enzo-gos <enzo.nguyen.gos@gmail.com>
2024-05-10 10:29:10 +0700

    fix: seeding duplicate twice

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,116 +1,69 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Ensure admin role exists
+admin_role = Role.find_or_create_by!(name: 'admin')
 
-admin_role = Role.create!(name: 'admin')
-Role.create!(name: 'user')
+# Ensure user role exists
+user_role = Role.find_or_create_by!(name: 'user')
 
-# generate some service of meeting room
-Facility.create!(name: 'Internet', description: 'Reliable and fast internet connection for seamless online activities during meetings')
-Facility.create!(name: 'Projector', description: 'A device for projecting images onto a screen or wall')
-Facility.create!(name: 'Whiteboard', description: 'A smooth, glossy surface for writing on with markers, typically used for presentations and brainstorming sessions')
-Facility.create!(name: 'Video Conferencing', description: 'Equipment and software for conducting remote meetings with audio and video capabilities')
-Facility.create!(name: 'Refreshments', description: 'Beverages and snacks provided for meeting attendees')
-Facility.create!(name: 'Flipchart', description: 'A large pad of paper attached to a stand, used for presenting information or brainstorming ideas with markers')
-Facility.create!(name: 'Comfortable Seating', description: 'Chairs designed for long periods of sitting, providing ergonomic support and comfort')
-Facility.create!(name: 'Climate Control', description: 'Heating, ventilation, and air conditioning systems to maintain a comfortable temperature')
+# Create facilities if they don't exist
+facilities_data = [
+  { name: 'Internet', description: 'Reliable and fast internet connection for seamless online activities during meetings' },
+  { name: 'Projector', description: 'A device for projecting images onto a screen or wall' },
+  { name: 'Whiteboard', description: 'A smooth, glossy surface for writing on with markers, typically used for presentations and brainstorming sessions' },
+  { name: 'Video Conferencing', description: 'Equipment and software for conducting remote meetings with audio and video capabilities' },
+  { name: 'Refreshments', description: 'Beverages and snacks provided for meeting attendees' },
+  { name: 'Flipchart', description: 'A large pad of paper attached to a stand, used for presenting information or brainstorming ideas with markers' },
+  { name: 'Comfortable Seating', description: 'Chairs designed for long periods of sitting, providing ergonomic support and comfort' },
+  { name: 'Climate Control', description: 'Heating, ventilation, and air conditioning systems to maintain a comfortable temperature' }
+]
+facilities_data.each do |facility_data|
+  Facility.find_or_create_by!(facility_data)
+end
 
-Department.create!(name: 'Golden Owl Q3', description: 'The department at CMT-8 street', address: '538 CMT 8 street, district 3 HCMC')
-Department.create!(name: 'Golden Owl Q2', description: 'The department at Sala', address: 'district 2 HCMC')
+# Create departments if they don't exist
+departments_data = [
+  { name: 'Golden Owl Q3', description: 'The department at CMT-8 street', address: '538 CMT 8 street, district 3 HCMC' },
+  { name: 'Golden Owl Q2', description: 'The department at Sala', address: 'district 2 HCMC' }
+]
+departments_data.each do |department_data|
+  Department.find_or_create_by!(department_data)
+end
 
-random_password = Devise.friendly_token[0, 20]
+# Create or find users
+users_data = [
+  { email: 'enzo.nguyen.gos@gmail.com', password: Devise.friendly_token[0, 20] },
+  { email: 'nducminh.it@gmail.com', password: '123123', roles: [admin_role] },
+  { email: 'dev1.gos@gmail.com', password: '123123' },
+  { email: 'dev2.gos@gmail.com', password: '123123' }
+]
+users_data.each do |user_data|
+  user = User.find_or_create_by!(email: user_data[:email]) do |u|
+    u.password = user_data[:password]
+    u.roles = user_data[:roles] || [user_role]
+  end
+end
 
-User.create!(
-  id: 2,
-  email: 'enzo.nguyen.gos@gmail.com',
-  password: random_password,
-  password_confirmation: random_password,
-  reset_password_token: nil,
-  reset_password_sent_at: nil,
-  remember_created_at: nil,
-  created_at: '2024-04-19 15:37:29.490271',
-  updated_at: '2024-04-19 15:37:29.490271',
-  firstname: nil,
-  lastname: nil,
-  provider: 'google',
-  uid: '113354911838389532580'
-)
-
-User.create!(
-  id: 1,
-  email: 'nducminh.it@gmail.com',
-  password: '123123',
-  password_confirmation: '123123',
-  reset_password_token: nil,
-  reset_password_sent_at: nil,
-  remember_created_at: nil,
-  created_at: '2024-04-17 03:17:33.787963',
-  updated_at: '2024-04-21 16:01:44.489933',
-  firstname: nil,
-  lastname: nil,
-  provider: nil,
-  uid: nil
-)
-
-User.create!(
-  id: 3,
-  email: 'dev1.gos@gmail.com',
-  password: '123123',
-  password_confirmation: '123123',
-  reset_password_token: nil,
-  reset_password_sent_at: nil,
-  remember_created_at: nil,
-  created_at: '2024-04-22 03:17:44.970778',
-  updated_at: '2024-04-22 03:17:44.970778',
-  firstname: nil,
-  lastname: nil,
-  provider: nil,
-  uid: nil
-)
-
-User.create!(
-  id: 4,
-  email: 'dev2.gos@gmail.com',
-  password: '123123',
-  password_confirmation: '123123',
-  reset_password_token: nil,
-  reset_password_sent_at: nil,
-  remember_created_at: nil,
-  created_at: '2024-05-02 03:24:23.25183',
-  updated_at: '2024-05-02 03:24:23.25183',
-  firstname: nil,
-  lastname: nil,
-  provider: nil,
-  uid: nil
-)
-
-# Create teams
-team_alpha = Team.create!(name: 'Ruby Alpha')
-team_beta = Team.create!(name: 'Ruby Beta')
-team_ld = Team.create!(name: 'L&D')
-
-# Find users
-user1 = User.find_by(email: 'enzo.nguyen.gos@gmail.com')
-user2 = User.find_by(email: 'nducminh.it@gmail.com')
-user3 = User.find_by(email: 'dev1.gos@gmail.com')
-user4 = User.find_by(email: 'dev2.gos@gmail.com')
+# Create or find teams
+teams_data = ['Ruby Alpha', 'Ruby Beta', 'L&D']
+teams_data.each do |team_name|
+  Team.find_or_create_by!(name: team_name)
+end
 
 # Assign users to teams
-user1.teams << team_alpha
-user2.teams << team_beta
-user3.teams << team_ld
-user4.teams << team_alpha  # You can assign a user to multiple teams if needed
+# Define user email and team name mappings
+user_team_mappings = {
+  'enzo.nguyen.gos@gmail.com' => 'Ruby Alpha',
+  'nducminh.it@gmail.com' => 'Ruby Beta',
+  'dev1.gos@gmail.com' => 'L&D',
+  'dev2.gos@gmail.com' => 'Ruby Alpha'
+}
 
-user2.roles << admin_role
+# Assign users to teams based on the mappings
+user_team_mappings.each do |email, team_name|
+  user = User.find_by(email: email)
+  team = Team.find_by(name: team_name)
 
-# Save the users to persist the associations to the database
-user1.save
-user2.save
-user3.save
-user4.save
+  # Check if the user is not already assigned to the team
+  unless user&.teams&.include?(team)
+    user&.teams << team if user && team
+  end
+end
