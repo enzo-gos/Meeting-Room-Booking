6f9f8fb9a864170bca100239f06e9af8de07e53a
enzo-gos <enzo.nguyen.gos@gmail.com>
2024-05-10 10:58:56 +0700

    fix: import consumer for production

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,4 +1,4 @@
-import consumer from "./consumer";
+import consumer from "channels/consumer";
 
 document.addEventListener("turbo:load", function () {
   const room_id = $("[data-room-id]").attr("data-room-id");
