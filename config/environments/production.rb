d7a973311942e626398b076f8146a89ad74fc659
enzo-gos <enzo.nguyen.gos@gmail.com>
2024-05-12 14:14:43 +0700

    config mail service for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,19 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: 'enzo.nguyen.gos@gmail.com', charset: 'utf-8' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: ENV['MAIL_SERVICE_USERNAME'],
+    password: ENV['MAIL_SERVICE_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
+  config.action_mailer.default_url_options = { host: 'https://gomeeting.onrender.com/' }
 end
