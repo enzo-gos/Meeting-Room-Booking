3b76f93d666e36e58b02ace790b0dcc317fae95c
enzo-gos <enzo.nguyen.gos@gmail.com>
2024-05-12 14:22:05 +0700

    config domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,16 +97,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = { from: 'enzo.nguyen.gos@gmail.com', charset: 'utf-8' }
+  config.action_mailer.default_options = { from: 'enzo.nguyen.gos@gmail.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
+    domain: 'google.com',
     user_name: ENV['MAIL_SERVICE_USERNAME'],
     password: ENV['MAIL_SERVICE_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true
   }
 
-  config.action_mailer.default_url_options = { host: 'https://gomeeting.onrender.com/' }
+  config.action_mailer.default_url_options = { host: 'gomeeting.onrender.com' }
 end
