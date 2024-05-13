be0ca75e31ee005ab90ecfd0c2ab7c7a4ec02149
enzo-gos <enzo.nguyen.gos@gmail.com>
2024-05-13 10:39:10 +0700

    change email domain

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = { from: 'enzo.nguyen.gos@gmail.com' }
+  config.action_mailer.default_options = { from: 'admin@mtech.id.vn' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = { from: 'enzo.nguyen.gos@gmail.com' }
+  config.action_mailer.default_options = { from: 'admin@mtech.id.vn' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: ENV['MAIL_SERVICE_PRODUCTION'],
